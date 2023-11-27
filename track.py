682aab3fe0d588faae5b1a3a126d2267e2bbbea8
Håkon Nese <haakon.nese@gmail.com>
2023-11-27 21:04:31 +0100

    sahi

--- track.py
+++ track.py
@@ -1,13 +1,25 @@
 from ultralytics import YOLO
+from ultralytics.trackers import BOTSORT, BYTETracker
 import json
 from utils.custom_annotator import BaseAnnotator, Detection, Color, KeypointAnnotator
 import cv2
 import numpy as np
 from utils.mot_metrics import motMetricsEnhancedCalculator
 from utils.case import VIDEO, video
+from sahi.predict  import get_sliced_prediction
+from sahi import AutoDetectionModel
+import os
+from PIL import Image
+import supervision as sv
 
 abs_positions = np.array([[52.5, 70-25.85, 0], [52.5, 70-44.15, 0], [52.5, 70-70, 0], [43.35, 70-35, 0], [61.65, 70-35, 0]], dtype=np.float32)
-model = YOLO("runs/detect/train7/weights/last.pt")
+model = AutoDetectionModel.from_pretrained(
+    model_type='yolov8',
+    model_path="runs/detect/train7/weights/last.pt",
+    confidence_threshold=0.25,
+    device="cuda:0",  # or 'cuda:0'
+)
+model2 = YOLO("runs/detect/train7/weights/last.pt")
 annotator = BaseAnnotator(colors=[Color(255, 255, 0), Color(0, 255, 255)], thickness=2)
 keypoint_annotator = KeypointAnnotator(colors=[Color(255, 255, 0), 
                                                Color(0, 255, 255),
@@ -17,7 +29,7 @@ keypoint_annotator = KeypointAnnotator(colors=[Color(255, 255, 0),
                                                Color(0, 0, 255),
                                                Color(0, 0, 0),], thickness=10)
 
-results = model.track(f"data/from_idun/{VIDEO}/img1", stream=True, device="cuda", tracker="botsort.yaml", persist=True)
+results = model2.track(f"data/from_idun/{VIDEO}/img1", stream=True, device="cuda", tracker="botsort.yaml", persist=True)
 size = (1920,1080)
 out = cv2.VideoWriter(f'outputs/result_{VIDEO}.mp4', cv2.VideoWriter_fourcc(*'mp4v'), 30, size)
 motoutput = ""
@@ -35,12 +47,101 @@ def focalLength_to_camera_matrix(focalLenght, image_size):
     ])
     return K
 camera_matrix = focalLength_to_camera_matrix(1, (1920, 1080))
+path = f"data/from_idun/{VIDEO}/img1"
+class dotdict(dict):
+    """dot.notation access to dictionary attributes"""
+    __getattr__ = dict.get
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
 
-for i, result in enumerate(results):
-    result_dict = json.loads(result.tojson())
+class Result:
+    def __init__(self, result):
+        num_pred = len(result.object_prediction_list)
+        xyxy = np.zeros((num_pred, 4))
+        conf = np.zeros(num_pred)
+        cls = np.zeros(num_pred)
+        for i, res in enumerate(result.to_coco_annotations()):
+            xyxy[i] = self.xywh_toxyxy(res["bbox"])
+            conf[i] = res["score"]
+            cls[i] = res["category_id"]
+        self.xyxy = xyxy
+        self.conf = conf
+        self.cls = cls
+
+    def xywh_toxyxy(self, xywh):
+        # print(xywh)
+        x = xywh[0]
+        y = xywh[1]
+        w = xywh[2]
+        h = xywh[3]
+        x1, y1 = x, y
+        x2, y2 = x+w, y+h
+        return [x1, y1, x2, y2]
+
+    def print(self):
+        return {
+            "xyxy": self.xyxy,
+            "conf": self.conf,
+            "cls": self.cls
+        }
+
+def tracker_result_to_json(tracker_result):
+    res_list = []
+    for res in tracker_result:
+        res_list.append({
+            "class": res[6],
+            "box": {
+                "x1": res[0],
+                "x2": res[2],
+                "y1": res[1],
+                "y2": res[3],
+            },
+            "confidence": res[5],
+            "track_id": res[4]
+        })
+    return res_list
+
+args = dotdict({"track_high_thresh": 0.5,
+                  "track_low_thresh": 0.1,
+                  "new_track_thresh": 0.6,
+                  "track_buffer": 30,
+                  "match_thresh": 0.8,
+                  "gmc_method": "sparseOptFlow",
+                  "proximity_thresh": 0.5,
+                  "appearance_thresh": 0.25
+                }
+
+                  )
+
+tracker = BOTSORT(args=args)
+import time
+start = time.perf_counter()
+for i, file in enumerate(sorted(os.listdir(path))):
+    print(time.perf_counter() - start)
+    start = time.perf_counter()
+    image = os.path.join(path, file)
+    print(image)
+    image = np.array(Image.open(image))
+    result = get_sliced_prediction(
+        image,
+        model,
+        slice_height = 540,
+        slice_width = 960,
+        overlap_height_ratio = 0.5,
+        overlap_width_ratio = 0.5
+    )
+    image = image[:, :, ::-1].copy()
+    # result2 = next(results)
+    # print("YOLO FInished")
+    result = Result(result)
+    # tracks = tracker.init_track(result.xyx, result.conf, result.cls, image)
+    # result = tracker.multi_predict(tracks)
+    result = tracker.update(result, image)
+    # print(result.print())
+    result_dict = tracker_result_to_json(result)
     for res in result_dict:
         motoutput += f"{i+1},{res['track_id']},{res['box']['x1']},{res['box']['y1']},{res['box']['x2']-res['box']['x1']},{res['box']['y2']-res['box']['y1']},1,{int(res['class'])+1},1.0\n"
-    detections = Detection.from_results(result_dict, names=model.names)
+    detections = Detection.from_results(result_dict, names=["ball", "player"])
     points_to_transform = []
     warp = False
     # calculate position on field for each player if keypoints are available
@@ -115,9 +216,9 @@ for i, result in enumerate(results):
             if detection.tracker_id in run_distance:
                 detection.distance = run_distance[detection.tracker_id]
     if str(i+1) in keypoints:
-        image = keypoint_annotator.annotate_points(annotator.annotate(image=result.orig_img.copy(), detections=detections), keypoints=keypoints[str(i+1)])
+        image = keypoint_annotator.annotate_points(annotator.annotate(image=image.copy(), detections=detections), keypoints=keypoints[str(i+1)])
     else:
-        image = annotator.annotate(image=result.orig_img.copy(), detections=detections)
+        image = annotator.annotate(image=image.copy(), detections=detections)
     out.write(image)
 
     # show why the distances are not calculated correctly
